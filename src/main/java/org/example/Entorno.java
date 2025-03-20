061bdb1c95bb468caa2249e0befd863a2f9e0fcb
Juan Daniel Ordoñez <ord24979@uvg.edu.gt>
2025-03-20 00:58:15 -0600

    Clase Entorno

---
+++ src/main/java/org/example/Entorno.java
@@ -0,0 +1,45 @@
+package org.example;
+import java.util.Map;
+import java.util.HashMap;
+
+public class Entorno {
+    private Map<String, Integer> variables = new HashMap<>(); // Variables (nombre -> valor)
+    private Map<String, DefFuncion> funciones = new HashMap<>(); // Funciones (nombre -> definición)
+    private Entorno parent; // Para manejar ámbitos anidados
+
+    public Entorno(Entorno parent) {  //constructor
+        this.parent = parent;
+    }
+
+    // Define una variable en entorno
+    public void defineVariable(String nombre, int value) {
+        variables.put(nombre, value);
+    }
+
+    //Obriene valor de alguna variable
+    public int getVariable(String nombre) {
+        if (variables.containsKey(nombre)) {
+            return variables.get(nombre);
+        } else if (parent != null) {
+            return parent.getVariable(nombre);
+        } else {
+            throw new RuntimeException("Variable no definida: " + nombre);
+        }
+    }
+
+    // Define una función en entorno
+    public void defineFunction(String nombre, DefFuncion funcion) {
+        funciones.put(nombre, funcion);
+    }
+
+    // Obtiene definición de funcion
+    public DefFuncion getFuncion(String nombre) {
+        if (funciones.containsKey(nombre)) {
+            return funciones.get(nombre);
+        } else if (parent != null) {
+            return parent.getFuncion(nombre);
+        } else {
+            throw new RuntimeException("Función no definida: " + nombre);
+        }
+    }
+}
